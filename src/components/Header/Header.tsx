53e127ed442aa11d85d54399dacf12b3f6c3b162
Seymour James <seymourpjames@gmail.com>
2022-07-21 23:39:42 -0700

    added mobile navbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,15 +2,13 @@ import React, { FC, useState } from "react";
 import { Link } from "react-router-dom";
 import classes from "./headerStyles.module.css";
 import DesktopNav from "./Nav/DesktopNav/DesktopNav";
+import MobileNav from "./Nav/MobileNav/MobileNav";
 
 const Header: FC = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-
-  console.log(isOpen);
-
   return (
     <header className={classes.header}>
       <DesktopNav />
+      <MobileNav />
     </header>
   );
 };
